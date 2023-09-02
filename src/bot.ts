d49293d054c79f97258a9018cc371fbbc34713bc
Bruno <brunobartunek1@gmail.com>
2023-09-02 07:08:39 +0200

    first commit

---
+++ src/bot.ts
@@ -0,0 +1,54 @@
+import { Bot } from "grammy";
+require('dotenv').config();
+
+interface ENV {
+    BOT_TOKEN: string | undefined;
+}
+
+interface Command {
+    command: string;
+    description: string;
+}
+
+interface Membership {
+    type: string;
+    price: number;
+    sticker: string; // URL or ID of sticker
+}
+
+const bot = new Bot(process.env.BOT_TOKEN as string);
+
+const commands: Command[] = [
+    { command: "/clenstvi", description: "Vypíše všechny druhy členství" },
+    { command: "/help", description: "Show all commands" },
+    { command: "hi", description: "Say hello" },
+    { command: "Send a sticker", description: "Receive a thumbs up" }
+];
+
+const memberships: Membership[] = [
+    { type: "Základní", price: 3000, sticker: "URL_OR_ID_OF_STICKER_1" },
+    { type: "All In One", price: 4000, sticker: "URL_OR_ID_OF_STICKER_2" },
+    { type: "Revolutio", price: 27000, sticker: "URL_OR_ID_OF_STICKER_3" }
+];
+
+const getCommandsList = (): string => {
+    return commands.map(cmd => `${cmd.command} - ${cmd.description}`).join('\n');
+}
+
+const getMembershipsList = (): string => {
+    return memberships.map(mem => `${mem.type}: ${mem.price} Kč`).join('\n');
+}
+
+bot.command("start", (ctx) => ctx.reply('Zdravím, jsem bot, který vám pomůže s členstvím v našem klubu. Napište /help pro seznam příkazů.'));
+bot.command("help", (ctx) => ctx.reply(`Tady máte seznam příkazů:\n${getCommandsList()}`));
+bot.command('clenstvi', (ctx) => {
+    ctx.reply(getMembershipsList());
+});
+
+await bot.api.setMyCommands([
+    { command: "start", description: "Start the bot" },
+    { command: "help", description: "Show help text" },
+    { command: "settings", description: "Open settings" },
+]);
+
+bot.start();
