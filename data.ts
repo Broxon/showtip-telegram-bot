3180f38d4b440dcef21d7bea3642b057aeae3e29
Aurelius Theodor Klíma <aureliust@seznam.cz>
2024-01-06 20:59:21 +0100

    prices

--- data.ts
+++ data.ts
@@ -11,14 +11,14 @@ export const commands: Command[] = [
 ];
 
 export const memberships = [
-    { type: "Základní členství", price: 2000, description: "Základní balíček pro jeden tiket", id: "price_1OVfIDIKOPne52YONylsYgNx", mode: "payment" },
-    { type: "All In One", price: 1500, description: "Členství na měsíc", id: "price_1OLqUdIKOPne52YOJVyQpP4c", mode: "subscription" },
+    { type: "Základní členství", price: 3000, description: "Základní balíček pro jeden tiket", id: "price_1OLqXsIKOPne52YOC2tr3F0j", mode: "payment" },
+    { type: "All In One", price: 2000, description: "Členství na měsíc", id: "price_1OVfeEIKOPne52YOgHiAzq1f", mode: "subscription" },
     { type: "Revolutio", price: 27000, description: "Nejlepší členství, garance vrácení peněz", id: "price_1OLqXLIKOPne52YOLu50Yh7J", mode: "payment" }
 ];
 
 export const names = [
-    "JEDNOTNÝ TIKET 🔥, 2000 CZK/TIKET",
-    "All IN ONE 🏆, 1500 CZK/MĚSÍC",
+    "JEDNOTNÝ TIKET 🔥, 3000 CZK/TIKET",
+    "All IN ONE 🏆, 2000 CZK/MĚSÍC",
     "REVOLUTIO 👑, 27000 CZK/10 TIKETŮ"
 ];
 
