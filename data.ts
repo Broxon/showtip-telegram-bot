630bbee03bc1b079cc887b6fb49d3c7aa8f2dac8
Bruno <brunobartunek1@gmail.com>
2024-07-04 12:50:01 +0200

    Feat/Free-trial

--- data.ts
+++ data.ts
@@ -19,7 +19,7 @@ export const memberships = [
 
 export const names = [
     "JEDNOTNÝ TIKET 🔥, 3000 CZK/TIKET",
-    "All IN ONE 🏆, 1500 CZK/MĚSÍC",
+    "All IN ONE 🏆, 0 CZK/3 DNY",
 ];
 
 export const paymentNames = [
